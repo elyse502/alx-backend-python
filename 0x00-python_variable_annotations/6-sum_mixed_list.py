30e21b2430489d6a957b75e1ba8a960b03e807a7
elyse502 <elyseniyibizi502@gmail.com>
2024-01-11 07:09:43 +0000

    Takes a list mxd_lst of integers and floats and returns their sum as a float

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Module 6-sum_mixed_list.py
+"""
+from typing import List, Union
+
+
+def sum_mixed_list(input_list: List[Union[int, float]]) -> float:
+    """
+    Takes a mixed list of integers and floats
+    and returns their sum
+    """
+    return sum(input_list)
