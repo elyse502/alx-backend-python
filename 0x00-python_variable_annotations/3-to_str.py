175f4370af3b4655e15fffba834c65d3f76bb7cf
elyse502 <elyseniyibizi502@gmail.com>
2024-01-11 07:01:20 +0000

    Returns the string representation of the float

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module 3-to_str.py
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Takes a float and returns
+    its string representation
+    """
+    return str(n)
